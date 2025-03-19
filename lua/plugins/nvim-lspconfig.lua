9865fdb30a1c8cc29c18e10dee07d47f640f6478
tirzasrwn <tirzasrwn@gmail.com>
2025-03-19 11:11:20 +0700

    feat: Add vimtex plugin and intelephense lsp.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -195,19 +195,23 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
-      --
-
+      html = {},
+      cssls = {},
+      ts_ls = {},
+      clangd = {},
+      jdtls = {},
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
+      },
+      intelephense = {},
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
